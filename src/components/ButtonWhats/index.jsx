b3f8b8cf5e81274a166e2445ca6aa5e58c8bee1c
Webster <webster.rafael2014@gmail.com>
2024-05-01 12:02:52 -0400

    New

--- src/components/ButtonWhats/index.jsx
+++ src/components/ButtonWhats/index.jsx
@@ -2,7 +2,7 @@ import { FaWhatsappSquare } from "react-icons/fa";
 
 export function ButtonWhats() {
     return (
-        <a href='https://api.whatsapp.com/send?phone=5547997261914&text=Ol%C3%A1,%20Gostaria%20de%20Saber%20mais' className='fixed text-black dark:text-ouro bottom-1 right-1 animate-bounce' target='blank'>
+        <a href='https://api.whatsapp.com/send?phone=5547997261914&text=Ol%C3%A1,%20Gostaria%20de%20Saber%20mais' className='fixed text-black dark:text-white bottom-1 right-1 animate-bounce z-30' target='blank'>
             <FaWhatsappSquare size={50} />
         </a>
     )
