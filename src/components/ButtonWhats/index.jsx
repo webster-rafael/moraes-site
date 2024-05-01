0dd69437cebdba95d5b0f47fce20cb705a8ac702
Webster <webster.rafael2014@gmail.com>
2024-05-01 12:10:39 -0400

    New

--- src/components/ButtonWhats/index.jsx
+++ src/components/ButtonWhats/index.jsx
@@ -1,9 +1,9 @@
-import { FaWhatsappSquare } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 export function ButtonWhats() {
     return (
-        <a href='https://api.whatsapp.com/send?phone=5547997261914&text=Ol%C3%A1,%20Gostaria%20de%20Saber%20mais' className='fixed text-black dark:text-white lg:dark:text-ouro bottom-1 lg:text-black right-1 animate-bounce z-30' target='blank'>
-            <FaWhatsappSquare size={50} />
+        <a href='https://api.whatsapp.com/send?phone=5547997261914&text=Ol%C3%A1,%20Gostaria%20de%20Saber%20mais' className='fixed text-green-600 rounded-full bg-white dark:text-ouro lg:dark:text-ouro bottom-1 lg:text-black right-1 animate-bounce z-30' target='blank'>
+            <IoLogoWhatsapp size={50} />
         </a>
     )
 }
