75df79c4ba011954af60bda655df69ce0ed2fbad
Webster <webster.rafael2014@gmail.com>
2024-05-13 13:29:00 -0400

    NEW

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -177,7 +177,7 @@ export default function Sobre() {
                   href='/'
                   className='text-white bg-ouro hover:bg-ouro/80 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
                 >
-                  Confirmar Foto
+                  Cadastrar
                 </Link>
             </form>
           </div>
